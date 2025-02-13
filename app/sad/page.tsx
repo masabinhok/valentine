49c8b24bef5eb924b9bee8cd9cb7d57fee12ae87
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-02-13 21:39:00 +0545

    design rejection page version1

--- app/sad/page.tsx
+++ app/sad/page.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      response to noo... yet to build
+    <div className='w-screen max-h-screen bg-cotton flex flex-row flex-wrap items-center justify-center overflow-hidden '>
+      {/* <img src="/gifs/backhug.gif" alt="mochimochigif" /> */}
+      <img src="/gifs/backmassage.gif" alt="mochimochigif" />
+      <img src="/gifs/bump.gif" alt="mochimochigif" />
+      <img src="/gifs/care.gif" alt="mochimochigif" />
+      <img src="/gifs/cuddle.gif" alt="mochimochigif" />
+      <img src="/gifs/kiss.gif" alt="mochimochigif" />
+      <img src="/gifs/kisss.gif" alt="mochimochigif" />
+      {/* <img src="/gifs/kissss.gif" alt="mochimochigif" /> */}
+      <img src="/gifs/save.gif" alt="mochimochigif" />
+      <img src="/gifs/setup.gif" alt="mochimochigif" />
+
+      <p className='absolute text-7xl font-bold text-candy text-center max-lg:text-5xl p-5 top-50 '>THIS COULD BE US
+      </p>
+      <span className='absolute text-center text-xl text-candy/50 top-96'>But you said NO</span>
     </div>
   )
 }
