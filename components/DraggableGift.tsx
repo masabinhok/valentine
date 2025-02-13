4bd0da1deac4db02f1d091fcaab4474d9703f9b4
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-02-13 22:02:29 +0545

    change letter

--- components/DraggableGift.tsx
+++ components/DraggableGift.tsx
@@ -19,7 +19,7 @@ const DraggableGift = ({ giftProp, opens = false }: { giftProp: StaticImageData 
         className="cursor-grab w-36 h-36 absolute z-20 "
         onClick={() => {
           if (opens) {
-            setLetterVisible(!letterVisible);
+            setLetterVisible(true);
           }
         }}
       >
@@ -34,14 +34,14 @@ const DraggableGift = ({ giftProp, opens = false }: { giftProp: StaticImageData 
       </motion.div>
       {letterVisible && (<div className="relative p-5 rounded-lg shadow-lg h-screen w-full">
         <div className="absolute max-w-[380px] bg-candy/20  p-10 rounded-sm right-10 font-pacifico ">
-          Hey you,
+          Hiiii,
 
-          I don’t really know how to say this without sounding silly, but here goes nothing—
+          I don’t really know how to say this without sounding silly, but here goes nothing, I guess.
           I have the softest, most innocent little crush on you. I catch myself wondering how your day was, hoping it was filled with smiles. Sometimes, I imagine running into you by accident (or maybe not by accident), just to steal a moment of your time.
 
           When you achieve something amazing, I want to be the first to say congratulations and celebrate with you. And when I see you, even if it’s just for a second, my heart does this tiny, excited jump that I pretend isn’t happening.
 
-          I know I’ve missed chances to say more, to do more, but for now, I just want you to know—you make the world a little brighter just by being in it.
+          I know I’ve missed chances to say more, to do more, but for now, I just want you to know, you make the world a little brighter just by being in it.
 
           That’s all.
 
